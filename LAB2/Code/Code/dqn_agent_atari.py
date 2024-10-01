fc8b00f6099e4834d8b7cadfec39ba45c16ae4ad
LKV <a23202418@gmail.com>
2024-10-02 05:25:36 +0800

    implement TODO

--- LAB2/Code/Code/dqn_agent_atari.py
+++ LAB2/Code/Code/dqn_agent_atari.py
@@ -4,19 +4,26 @@ import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 from base_agent import DQNBaseAgent
 from models.atari_model import AtariNetDQN
-import gym
+import gymnasium as gym
 import random
+from gymnasium.wrappers import GrayScaleObservation, ResizeObservation, FrameStack
 
 class AtariDQNAgent(DQNBaseAgent):
 	def __init__(self, config):
 		super(AtariDQNAgent, self).__init__(config)
 		### TODO ###
 		# initialize env
-		# self.env = ???
+		self.env = gym.make(config["env_id"])
+		self.env = GrayScaleObservation(self.env)
+		self.env = ResizeObservation(self.env, 84)
+		self.env = FrameStack(self.env, 4)
 
 		### TODO ###
 		# initialize test_env
-		# self.test_env = ???
+		self.test_env = gym.make(config["env_id"])
+		self.test_env = GrayScaleObservation(self.test_env)
+		self.test_env = ResizeObservation(self.test_env, 84)
+		self.test_env = FrameStack(self.test_env, 4)
 
 		# initialize behavior network and target network
 		self.behavior_net = AtariNetDQN(self.env.action_space.n)
@@ -32,14 +39,14 @@ class AtariDQNAgent(DQNBaseAgent):
 		### TODO ###
 		# get action from behavior net, with epsilon-greedy selection
 		
-		# if random.random() < epsilon:
-		# 	action = ???
-		# else:
-		# 	action = ???
+		if random.random() < epsilon:
+			action = action_space.sample()
+		else:
+			action = self.behavior_net(torch.tensor(observation).float().to(self.device).unsqueeze(0)).argmax().item()
 
-		# return action
+		return action
 
-		return NotImplementedError
+		# return NotImplementedError
 	
 	def update_behavior_network(self):
 		# sample a minibatch of transitions
@@ -53,22 +60,21 @@ class AtariDQNAgent(DQNBaseAgent):
 		# 4. calculate loss between Q(s,a) and Q_target
 		# 5. update behavior net
 
-		
-		# q_value = ???
-		# with torch.no_grad():
-			# q_next = ???
+		q_value = torch.gather(self.behavior_net(state), 1, action.long())
+		with torch.no_grad():
+			q_next = self.target_net(next_state).max(1).values
 
 			# if episode terminates at next_state, then q_target = reward
-			# q_target = ???
+			q_target = torch.where(done.bool(), reward, reward + self.gamma * q_next)
         
 		
-		# criterion = ???
-		# loss = criterion(q_value, q_target)
+		criterion = nn.MSELoss()
+		loss = criterion(q_value, q_target)
 
-		# self.writer.add_scalar('DQN/Loss', loss.item(), self.total_time_step)
+		self.writer.add_scalar('DQN/Loss', loss.item(), self.total_time_step)
 
-		# self.optim.zero_grad()
-		# loss.backward()
-		# self.optim.step()
+		self.optim.zero_grad()
+		loss.backward()
+		self.optim.step()
 	
 	
--- LAB2/Code/Code/main.py
+++ LAB2/Code/Code/main.py
@@ -12,13 +12,16 @@ if __name__ == '__main__':
 		"eps_decay": 1000000,
 		"eval_epsilon": 0.01,
 		"replay_buffer_capacity": 100000,
-		"logdir": 'log/DQN/Enduro/',
+		# "logdir": 'log/DQN/Enduro/',
+		"logdir": 'log/DQN/MsPacman/',
 		"update_freq": 4,
 		"update_target_freq": 10000,
 		"learning_rate": 0.0000625,
         "eval_interval": 100,
         "eval_episode": 5,
-		"env_id": 'ALE/Enduro-v5',
+		# "env_id": 'ALE/Enduro-v5',
+		"env_id": 'ALE/MsPacman-v5',
+        # todo seed
 	}
     agent = AtariDQNAgent(config)
     agent.train()
